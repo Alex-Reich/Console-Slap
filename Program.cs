d26819f31f679aeff9d735c6f677f7d50fc3fbb7
Alex Reich <atrain.reich@gmail.com>
2018-06-13 15:26:38 -0600

    Working Game

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using csharp_intro.Models;
+
+namespace csharp_intro
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var playing = true;
+
+      var game = new BatmanGame();
+      while (playing)
+      {
+        game.Play();
+        Console.WriteLine("Gotham still needs you keep fighting? Y/n");
+        if (Console.Read() == 'n') { playing = false; };
+      }
+
+
+
+    }
+  }
+}
